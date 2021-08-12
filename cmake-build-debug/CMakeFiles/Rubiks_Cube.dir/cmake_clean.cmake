bc6c71ee5bdf3aadeb83314d284db54e06a3384b
Ayden McNeil <ayden.mcneil@gmail.com>
2021-08-12 13:01:20 -0500

    first commit

---
+++ cmake-build-debug/CMakeFiles/Rubiks_Cube.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Rubiks_Cube.dir/main.cpp.obj"
+  "Rubiks_Cube.exe"
+  "Rubiks_Cube.exe.manifest"
+  "Rubiks_Cube.lib"
+  "Rubiks_Cube.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Rubiks_Cube.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
